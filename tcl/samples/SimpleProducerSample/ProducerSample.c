8f5cf47dd57a6c2fbc68d8e0d20f186d9de3f8d1
Eyal Ilsar <eyali@qce.qualcomm.com>
2014-01-13 16:42:15 +0200

    AJNSF-35 - Fix inclusion of Linux only sample application code
    Change-Id: I4c1a8d30fdc8163e5f62065c007d24ace8b75bce
    Signed-off-by: Eyal Ilsar <eyali@qce.qualcomm.com>

--- tcl/samples/SimpleProducerSample/ProducerSample.c
+++ tcl/samples/SimpleProducerSample/ProducerSample.c
@@ -17,7 +17,7 @@
 #include <NotificationProducerSample.h>
 #include <alljoyn/notification/NotificationProducer.h>
 
-#ifdef QCC_OS_LINUX
+#ifdef __linux
 #include <producer_sample_util.h>
 #else
 #define GetNotificationFromUser(x) do { } while (0)
