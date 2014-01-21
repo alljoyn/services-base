75935aa2ccb6693af8aba9d93a894e30b4cb0f66
Eyal Ilsar <eyali@qce.qualcomm.com>
2014-01-22 01:02:56 +0200

    AJCPSF-37 - remove extern AJ_BusAttachment in ControlPanelService.h
    Change-Id: I86b8ed5a43602a3777167987d4a7fb6faa7760a0
    Signed-off-by: Eyal Ilsar <eyali@qce.qualcomm.com>

--- tcl/inc/alljoyn/controlpanel/ControlPanelService.h
+++ tcl/inc/alljoyn/controlpanel/ControlPanelService.h
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2013, AllSeen Alliance. All rights reserved.
+ * Copyright (c) 2013 - 2014, AllSeen Alliance. All rights reserved.
  *
  *    Permission to use, copy, modify, and/or distribute this software for any
  *    purpose with or without fee is hereby granted, provided that the above
@@ -36,18 +36,6 @@
 
 #define CONTROLPANEL_ANNOUNCEOBJECTS \
     CONTROLPANELANNOUNCEOBJECTS
-
-/**
- * "boolean" that reflects whether the bus is connected or not
- * Can be set to TRUE or FALSE - should default to FALSE
- */
-extern uint8_t isBusConnected;
-
-/**
- * busAttachment that will be used for connecting and sending messages
- * Needs to be defined in app
- */
-extern AJ_BusAttachment busAttachment;
 
 /**
  * Port used for controlpanelservice
