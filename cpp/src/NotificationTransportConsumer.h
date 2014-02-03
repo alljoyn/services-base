c4a25a94a4e8dd6e6bde0a7524c269b7e8d6b8fc
Lior Gdaliahu <liorg@qce.qualcomm.com>
2014-02-03 15:23:18 +0200

    Adjustments for mobile wrappers
    Change-Id: Ie6581a7bf97d45a8e9b63fe270877da912a4e797
    Signed-off-by: Lior Gdaliahu <liorg@qce.qualcomm.com>

--- cpp/src/NotificationTransportConsumer.h
+++ cpp/src/NotificationTransportConsumer.h
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2013, AllSeen Alliance. All rights reserved.
+ * Copyright (c) 2013-2014, AllSeen Alliance. All rights reserved.
  *
  *    Permission to use, copy, modify, and/or distribute this software for any
  *    purpose with or without fee is hereby granted, provided that the above
@@ -21,6 +21,7 @@
 #include <pthread.h>
 
 #include "NotificationTransport.h"
+#include <pthread.h>
 
 namespace ajn {
 namespace services {
