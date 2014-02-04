0ca5489166a087f5b986dd4fbf54c1191e4ea9ea
Eyal Ilsar <eyali@qce.qualcomm.com>
2014-02-04 14:28:49 +0200

    AJCPSF-40 - The functions getboolval and setboolval have been removed from Controlpanelprovided.h
    Change-Id: I7fce904e23f18e69978fe722b1c2ca6e2a0ef6a8
    Signed-off-by: Eyal Ilsar <eyali@qce.qualcomm.com>

--- tcl/samples/CPSControlleeSample/inc/ControlPanelProvided.h
+++ tcl/samples/CPSControlleeSample/inc/ControlPanelProvided.h
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2013, AllSeen Alliance. All rights reserved.
+ * Copyright (c) 2013 - 2014, AllSeen Alliance. All rights reserved.
  *
  *    Permission to use, copy, modify, and/or distribute this software for any
  *    purpose with or without fee is hereby granted, provided that the above
@@ -60,6 +60,10 @@ void setdoubleVar(double newdoubleVar);
 
 void* getStringVar();
 void setStringVar(const char* newStringVar);
+
+/* For Consistency with C++ code */
+#define getboolVar getuint32Var
+#define setboolVar setuint32Var
 
 void startOven();
 void stopOven();
