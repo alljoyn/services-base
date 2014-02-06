4a2960c0e3c596edc0298d92e20509faf5c97831
Eyal Ilsar <eyali@qce.qualcomm.com>
2014-02-06 22:34:57 +0200

    AJCOMSF-7 - Separate Sample code from service code
    Change-Id: Ibb7ae5aeba63464d59a0976da09327ab9669d46c
    Signed-off-by: Eyal Ilsar <eyali@qce.qualcomm.com>

--- tcl/src/OnboardingService.c
+++ tcl/src/OnboardingService.c
@@ -15,6 +15,7 @@
  ******************************************************************************/
 
 #include <alljoyn/onboarding/OnboardingService.h>
+#include <alljoyn/onboarding/OnboardingControllerAPI.h>
 #include <aj_nvram.h>
 #include <aj_link_timeout.h>
 #include <aj_wifi_ctrl.h>
@@ -32,9 +33,9 @@ static const char* const OnboardingInterface[] = {
     NULL
 };
 
-static const uint16_t OnboardingVersion = 1;
+static const uint16_t AJSVC_OnboardingVersion = 1;
 
-const AJ_InterfaceDescription OnboardingInterfaces[] = {
+const AJ_InterfaceDescription AJSVC_OnboardingInterfaces[] = {
     AJ_PropertiesIface,
     OnboardingInterface,
     NULL
@@ -49,56 +50,64 @@ const AJ_InterfaceDescription OnboardingInterfaces[] = {
    static uint16_t obsSessionId = 0;
  */
 
-/* SoftAP state variable for AJTCL */
-extern uint8_t IsSoftAP;
-
 /*
  * Handles a property GET request so marshals the property value to return
  */
-AJ_Status OnboardingPropGetHandler(AJ_Message* replyMsg, uint32_t propId, void* context)
+AJ_Status AJOBS_PropGetHandler(AJ_Message* replyMsg, uint32_t propId, void* context)
 {
+    AJ_Status status = AJ_OK;
+    AJ_Arg structure;
+    const AJOBS_Error* obError = AJOBS_GetError();
     if (propId == OBS_VERSION_PROP) {
-        uint16_t q = OnboardingVersion;
-        return AJ_MarshalArgs(replyMsg, "q", q);
+        status = AJ_MarshalArgs(replyMsg, "q", AJSVC_OnboardingVersion);
     } else if (propId == OBS_STATE_PROP) {
-        return AJ_MarshalArgs(replyMsg, "n", obState);
+        status = AJ_MarshalArgs(replyMsg, "n", AJOBS_GetState());
     } else if (propId == OBS_LASTERROR_PROP) {
-        AJ_Status status = AJ_OK;
-        AJ_Arg structure;
         status = AJ_MarshalContainer(replyMsg, &structure, AJ_ARG_STRUCT);
-        status = AJ_MarshalArgs(replyMsg, "n", obLastError.code);
-        status = AJ_MarshalArgs(replyMsg, "s", obLastError.message);
+        if (status != AJ_OK) {
+            goto Exit;
+        }
+        status = AJ_MarshalArgs(replyMsg, "ns", obError->code, obError->message);
+        if (status != AJ_OK) {
+            goto Exit;
+        }
         status = AJ_MarshalCloseContainer(replyMsg, &structure);
-        return status;
     } else {
-        return AJ_ERR_UNEXPECTED;
+        status = AJ_ERR_UNEXPECTED;
     }
+
+Exit:
+
+    return status;
 }
 
 /*
  * Handles a property SET request and returns an unexpected error as all properties are declared as readonly
  */
-AJ_Status OnboardingPropSetHandler(AJ_Message* replyMsg, uint32_t propId, void* context)
+AJ_Status AJOBS_PropSetHandler(AJ_Message* replyMsg, uint32_t propId, void* context)
 {
     return AJ_ERR_UNEXPECTED;
 }
 
-AJ_Status OBS_ConfigureWiFi(AJ_Message*msg)
+AJ_Status AJOBS_ConfigureWiFiHandler(AJ_Message* msg)
 {
     AJ_Status status = AJ_OK;
     AJ_Message reply;
+    AJOBS_Info newInfo;
+    char* ssid;
+    char* pc;
+    size_t ssidLen;
+    size_t pcLen;
+    int16_t retVal;
 
     // Set provided network configuration
     AJ_Printf("ConfigureWiFi()\n");
 
-    OBInfo newInfo;
-    char* ssid;
-    char* pc;
     status = AJ_UnmarshalArgs(msg, "ssn", &ssid, &pc, &newInfo.authType);
     if (status != AJ_OK) {
         return status;
     }
-    if ((int8_t)newInfo.authType >= MAX_OF_WIFI_AUTH_TYPE || (int8_t)newInfo.authType <= MIN_OF_WIFI_AUTH_TYPE) {
+    if ((int8_t)newInfo.authType >= AJOBS_AUTH_TYPE_MAX_OF_WIFI_AUTH_TYPE || (int8_t)newInfo.authType <= AJOBS_AUTH_TYPE_MIN_OF_WIFI_AUTH_TYPE) {
         AJ_Printf("Unknown authentication type %d\n", newInfo.authType);
         status = AJ_MarshalErrorMsg(msg, &reply, AJSVC_ERROR_INVALID_VALUE);
         if (status != AJ_OK) {
@@ -110,15 +119,15 @@ AJ_Status OBS_ConfigureWiFi(AJ_Message*msg)
         }
         return status;
     }
-    size_t ssidLen = min(strlen(ssid), SSID_MAX_LENGTH);
-    strncpy(newInfo.ssid, ssid, SSID_MAX_LENGTH);
+    ssidLen = min(strlen(ssid), AJOBS_SSID_MAX_LENGTH);
+    strncpy(newInfo.ssid, ssid, AJOBS_SSID_MAX_LENGTH);
     newInfo.ssid[ssidLen] = '\0';
-    size_t pcLen = min(strlen(pc), PASSCODE_MAX_LENGTH);
-    strncpy(newInfo.pc, pc, PASSCODE_MAX_LENGTH);
+    pcLen = min(strlen(pc), AJOBS_PASSCODE_MAX_LENGTH);
+    strncpy(newInfo.pc, pc, AJOBS_PASSCODE_MAX_LENGTH);
     newInfo.pc[pcLen] = '\0';
 
     AJ_Printf("Got new info for %s with passcode=%s and auth=%d\n", newInfo.ssid, newInfo.pc, newInfo.authType);
-    int16_t retVal = 1;
+    retVal = 1;
     status = AJ_MarshalReplyMsg(msg, &reply);
     if (status != AJ_OK) {
         return status;
@@ -132,24 +141,24 @@ AJ_Status OBS_ConfigureWiFi(AJ_Message*msg)
         return status;
     }
 
-    newInfo.state = CONFIGURED_NOT_VALIDATED;
-    status = OBS_WriteInfo(&newInfo);
+    newInfo.state = AJOBS_STATE_CONFIGURED_NOT_VALIDATED;
+    status = AJOBS_SetInfo(&newInfo);
 
     if (status == AJ_OK) {
         // Change state to CONFIGURED
-        obState = newInfo.state;
+        AJOBS_SetState(newInfo.state);
     }
 
     return status;
 }
 
-AJ_Status OBS_ConnectWiFi(AJ_Message*msg)
+AJ_Status AJOBS_ConnectWiFiHandler(AJ_Message* msg)
 {
     AJ_Status status = AJ_OK;
 
     AJ_Printf("Got connect\n");
-    OBInfo obInfo;
-    status = OBS_ReadInfo(&obInfo);
+    AJOBS_Info obInfo;
+    status = AJOBS_GetInfo(&obInfo);
     if (status != AJ_OK) {
         return status;
     }
@@ -159,12 +168,12 @@ AJ_Status OBS_ConnectWiFi(AJ_Message*msg)
     return status;
 }
 
-AJ_Status OBS_OffboardWiFi(AJ_Message*msg)
+AJ_Status AJOBS_OffboardWiFiHandler(AJ_Message* msg)
 {
     AJ_Status status = AJ_OK;
 
     AJ_Printf("Offboard()\n");
-    status = OBCAPI_DoOffboardWiFi();
+    status = AJOBS_ControllerAPI_DoOffboardWiFi();
     if (status != AJ_OK) {
         return status;
     }
@@ -173,12 +182,13 @@ AJ_Status OBS_OffboardWiFi(AJ_Message*msg)
     return status;
 }
 
-AJ_Status OBS_GetScanInfo(AJ_Message*msg)
+AJ_Status AJOBS_GetScanInfoHandler(AJ_Message* msg)
 {
     AJ_Status status = AJ_OK;
     AJ_Message reply;
     AJ_Arg array;
     AJ_Arg structure;
+    uint32_t elapsed;
 
     AJ_Printf("GetScanInfo()\n");
 
@@ -187,7 +197,7 @@ AJ_Status OBS_GetScanInfo(AJ_Message*msg)
         return status;
     }
 
-    uint32_t elapsed = AJ_GetElapsedTime(&obLastScan, TRUE);
+    elapsed = AJ_GetElapsedTime(AJOBS_GetLastScanTime(), TRUE);
     if (elapsed > 0) {
         elapsed /= 60000;
     }
@@ -201,16 +211,16 @@ AJ_Status OBS_GetScanInfo(AJ_Message*msg)
     }
 
     int i = 0;
-    for (; i < obScanInfoCount; ++i) {
+    for (; i < AJOBS_GetScanInfoCount(); ++i) {
         status = AJ_MarshalContainer(&reply, &structure, AJ_ARG_STRUCT);
         if (status != AJ_OK) {
             return status;
         }
-        status = AJ_MarshalArgs(&reply, "s", obScanInfos[i].ssid);
+        status = AJ_MarshalArgs(&reply, "s", (AJOBS_GetScanInfos())[i].ssid);
         if (status != AJ_OK) {
             return status;
         }
-        status = AJ_MarshalArgs(&reply, "n", obScanInfos[i].authType);
+        status = AJ_MarshalArgs(&reply, "n", (AJOBS_GetScanInfos())[i].authType);
         if (status != AJ_OK) {
             return status;
         }
@@ -234,13 +244,14 @@ AJ_Status OBS_GetScanInfo(AJ_Message*msg)
 
 /*
    //will be used in future versions
-   AJ_Status OBS_SendConnectionResult(AJ_BusAttachment* bus)
+   AJ_Status AJOBS_SendConnectionResult(AJ_BusAttachment* bus)
    {
     AJ_Status status = AJ_OK;
-    AJ_Printf("ConnectionResult()\n");
+    const AJOBS_Error* obError = AJOBS_GetError();
     AJ_Message out;
     AJ_Arg structure;
-    status = AJ_MarshalSignal(bus, &out, OBS_CONNECTION_RESULT, NULL, obsSessionId, AJ_FLAG_GLOBAL_BROADCAST, CONNECTION_RESULT_TTL);
+    AJ_Printf("ConnectionResult()\n");
+    status = AJ_MarshalSignal(bus, &out, OBS_CONNECTION_RESULT, NULL, obsSessionId, AJ_FLAG_GLOBAL_BROADCAST, AJOBS_CONNECTION_RESULT_TTL);
     if (status != AJ_OK) {
         return status;
     }
@@ -248,11 +259,11 @@ AJ_Status OBS_GetScanInfo(AJ_Message*msg)
     if (status != AJ_OK) {
         return status;
     }
-    status = AJ_MarshalArgs(&out, "n", obLastError.code);
+    status = AJ_MarshalArgs(&out, "n", obError->code);
     if (status != AJ_OK) {
         return status;
     }
-    status = AJ_MarshalArgs(&out, "s", obLastError.message);
+    status = AJ_MarshalArgs(&out, "s", obError->message);
     if (status != AJ_OK) {
         return status;
     }
