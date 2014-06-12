a666abe495fc82f95409f3c025bc14affc6e98b8
rachell <rachell@qce.qualcomm.com>
2014-06-12 14:23:53 +0300

    ASABASE-195: Onboarding client crashes after trying to onboard following a factory reset. Change IntentAction to contain the package name.
    Change-Id: I6ae101614fc8c51cac0c926e7ae77b925c3d5910
    Signed-off-by: rachell <rachell@qce.qualcomm.com>

--- onboarding/java/sample_applications/android/OnboardingSampleClient/src/org/alljoyn/onboarding/test/Keys.java
+++ onboarding/java/sample_applications/android/OnboardingSampleClient/src/org/alljoyn/onboarding/test/Keys.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2013, AllSeen Alliance. All rights reserved.
+ * Copyright (c) 2013-2014, AllSeen Alliance. All rights reserved.
  *
  *    Permission to use, copy, modify, and/or distribute this software for any
  *    purpose with or without fee is hereby granted, provided that the above
@@ -22,66 +22,68 @@ package org.alljoyn.onboarding.test;
  */
 public class Keys {
 
-	class Actions {
+    public static final String actionPrefix = new Keys().getClass().getPackage().getName() + ".";
 
-		/**
-		 * An action indicating an error has occurred.
-		 * Has an extra string called Keys.Extras.EXTRA_ERROR provides the error msg.
-		 */
-		public static final String ACTION_ERROR = "ACTION_ERROR";
-		
-		/**
-		 * An action indicating a new device was found.
-		 * Has an extra string called Keys.Extras.EXTRA_DEVICE_ID provides the device id.
-		 */
-		public static final String ACTION_DEVICE_FOUND = "ACTION_DEVICE_FOUND";
-		
-		/**
-		 * An action indicating a device was lost.
-		 * Has an extra string called Keys.Extras.EXTRA_BUS_NAME provides the device bus name.
-		 */
-		public static final String ACTION_DEVICE_LOST = "ACTION_DEVICE_LOST";
-		
-		/**
-		 * An action indicating we are connected to a network. Has an extra string
-		 * called Keys.Extras.EXTRA_NETWORK_SSID provides the network ssid.
-		 * Id we are connecting/connected to a network, the EXTRA_NETWORK_SSID indicates that.
-		 */
-		public static final String ACTION_CONNECTED_TO_NETWORK = "ACTION_CONNECTED_TO_NETWORK";
+    static class Actions {
 
-		/**
-		 * An action indicating the password we gave do not match to the device password. 
-		 */
-		public static final String ACTION_PASSWORD_IS_INCORRECT = "ACTION_PASSWORD_IS_INCORRECT";
-	}
-	
-	class Extras {
-		
-		/**
-		 * The lookup key for a String object indicating the error message that occurred.
-		 * Retrieve with getStringExtra(String).
-		 */
-		public static final String EXTRA_ERROR = "extra_error";
-		
-		/**
-		 * The lookup key for a String object indicating a device id.
-		 * Retrieve with intent.getExtras().getString(String).
-		 * To get the device itself call getApplication().getDevice(String);
-		 */
-		public static final String EXTRA_DEVICE_ID = "extra_deviceId";
-		
-		/**
-		 * The lookup key for a String object indicating a device bus name.
-		 * Retrieve with getExtras().getString(String).
-		 */
-		public static final String EXTRA_BUS_NAME = "extra_BusName";
-		
-		/**
-		 * The lookup key for a String object indicating the network ssid to which we are connected.
-		 * Retrieve with intent.getStringExtra(String).
-		 */
-		public static final String EXTRA_NETWORK_SSID = "extra_network_ssid";
-		
-	}
-	
+        /**
+         * An action indicating an error has occurred.
+         * Has an extra string called Keys.Extras.EXTRA_ERROR provides the error msg.
+         */
+        public static final String ACTION_ERROR = actionPrefix + "ACTION_ERROR";
+
+        /**
+         * An action indicating a new device was found.
+         * Has an extra string called Keys.Extras.EXTRA_DEVICE_ID provides the device id.
+         */
+        public static final String ACTION_DEVICE_FOUND = actionPrefix + "ACTION_DEVICE_FOUND";
+
+        /**
+         * An action indicating a device was lost.
+         * Has an extra string called Keys.Extras.EXTRA_BUS_NAME provides the device bus name.
+         */
+        public static final String ACTION_DEVICE_LOST = actionPrefix + "ACTION_DEVICE_LOST";
+
+        /**
+         * An action indicating we are connected to a network. Has an extra string
+         * called Keys.Extras.EXTRA_NETWORK_SSID provides the network ssid.
+         * Id we are connecting/connected to a network, the EXTRA_NETWORK_SSID indicates that.
+         */
+        public static final String ACTION_CONNECTED_TO_NETWORK = actionPrefix + "ACTION_CONNECTED_TO_NETWORK";
+
+        /**
+         * An action indicating the password we gave do not match to the device password.
+         */
+        public static final String ACTION_PASSWORD_IS_INCORRECT = actionPrefix + "ACTION_PASSWORD_IS_INCORRECT";
+    }
+
+    class Extras {
+
+        /**
+         * The lookup key for a String object indicating the error message that occurred.
+         * Retrieve with getStringExtra(String).
+         */
+        public static final String EXTRA_ERROR = "extra_error";
+
+        /**
+         * The lookup key for a String object indicating a device id.
+         * Retrieve with intent.getExtras().getString(String).
+         * To get the device itself call getApplication().getDevice(String);
+         */
+        public static final String EXTRA_DEVICE_ID = "extra_deviceId";
+
+        /**
+         * The lookup key for a String object indicating a device bus name.
+         * Retrieve with getExtras().getString(String).
+         */
+        public static final String EXTRA_BUS_NAME = "extra_BusName";
+
+        /**
+         * The lookup key for a String object indicating the network ssid to which we are connected.
+         * Retrieve with intent.getStringExtra(String).
+         */
+        public static final String EXTRA_NETWORK_SSID = "extra_network_ssid";
+
+    }
+
 }
