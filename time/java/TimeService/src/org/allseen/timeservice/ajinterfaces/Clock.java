845900f1027ced56ca0524f6358037b15a068296
irivin <irivin@qce.qualcomm.com>
2014-09-10 17:46:12 +0300

    Initial commit for TimeService - Java
    Change-Id: I1c8b7322bc6b43c0d2b7c1a76893672a714f7c64
    Signed-off-by: irivin <irivin@qce.qualcomm.com>
    Signed-off-by: yitzhakf <yitzhakf@qce.qualcomm.com>

---
+++ time/java/TimeService/src/org/allseen/timeservice/ajinterfaces/Clock.java
@@ -0,0 +1,73 @@
+ /******************************************************************************
+  * Copyright (c) 2014, AllSeen Alliance. All rights reserved.
+  *
+  *    Permission to use, copy, modify, and/or distribute this software for any
+  *    purpose with or without fee is hereby granted, provided that the above
+  *    copyright notice and this permission notice appear in all copies.
+  *
+  *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+  *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+  *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+  *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+  *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+  *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+  ******************************************************************************/
+
+package org.allseen.timeservice.ajinterfaces;
+
+import org.alljoyn.bus.BusException;
+import org.alljoyn.bus.BusObject;
+import org.alljoyn.bus.annotation.BusInterface;
+import org.alljoyn.bus.annotation.BusProperty;
+import org.alljoyn.bus.annotation.Secure;
+import org.allseen.timeservice.TimeServiceConst;
+
+/**
+ * Time Service Clock interface
+ */
+@BusInterface(name=Clock.IFNAME)
+@Secure
+public interface Clock extends BusObject {
+
+    /**
+     * The name of the AllJoyn interface
+     */
+    public static final String IFNAME = TimeServiceConst.IFNAME_PREFIX + ".Clock";
+
+    /**
+     * This interface version
+     */
+    public static final short VERSION = 1;
+
+    /**
+     * Returns the interface version
+     * @return interface version number
+     * @throws BusException
+     */
+    @BusProperty(signature="q")
+    short getVersion() throws BusException;
+
+    /**
+     * Returns the DateTime object of the {@link Clock}
+     * @return {@link DateTimeAJ}
+     * @throws BusException
+     */
+    @BusProperty(signature="((qyy)(yyyq)n)")
+    DateTimeAJ getDateTime() throws BusException;
+
+    /**
+     * Set DateTime to the {@link Clock}
+     * @param dateTime
+     * @throws BusException
+     */
+    @BusProperty(signature="((qyy)(yyyq)n)")
+    void setDateTime(DateTimeAJ dateTime) throws BusException;
+
+    /**
+     * @return Return whether the {@link Clock} has ever been defined before
+     * @throws BusException
+     */
+    @BusProperty(signature="b")
+    boolean getIsSet() throws BusException;
+}
---
+++ time/java/TimeService/src/org/allseen/timeservice/server/ClockBusObj.java
@@ -0,0 +1,67 @@
+/******************************************************************************
+  * Copyright (c) 2014, AllSeen Alliance. All rights reserved.
+  *
+  *    Permission to use, copy, modify, and/or distribute this software for any
+  *    purpose with or without fee is hereby granted, provided that the above
+  *    copyright notice and this permission notice appear in all copies.
+  *
+  *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+  *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+  *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+  *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+  *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+  *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+  ******************************************************************************/
+
+package org.allseen.timeservice.server;
+
+import org.alljoyn.about.AboutService;
+import org.alljoyn.about.AboutServiceImpl;
+import org.allseen.timeservice.TimeServiceException;
+import org.allseen.timeservice.ajinterfaces.Clock;
+
+import android.util.Log;
+
+/**
+ * This class implements {@link Clock} interface and realizes AllJoyn communication
+ * with this clock.
+ */
+class ClockBusObj extends BaseClockBusObj implements Clock {
+    private static final String TAG = "ajts" + ClockBusObj.class.getSimpleName();
+
+    /**
+     * Prefix of the Clock object path
+     */
+    private static final String OBJ_PATH_PREFIX = "/Clock";
+
+    /**
+     * Constructor
+     * @param clock Clock events delegate. This clock receives all the {@link org.allseen.timeservice.server.Clock}
+     * related events.
+     */
+    ClockBusObj(org.allseen.timeservice.server.Clock clock) throws TimeServiceException {
+
+        super(clock, GlobalStringSequencer.append(OBJ_PATH_PREFIX));
+        AboutServiceImpl.getInstance().addObjectDescription(getObjectPath(), new String[]{Clock.IFNAME});
+    }
+
+
+    /**
+     * @see org.allseen.timeservice.server.BaseClockBusObj#release()
+     */
+    @Override
+    protected void release() {
+
+        super.release();
+
+        AboutService about = AboutServiceImpl.getInstance();
+
+        //Prevent exception in situation where About server was closed by the application.
+        if ( about.isServerRunning() ) {
+
+            Log.d(TAG, "Remove object description of: " + Clock.IFNAME);
+            about.removeObjectDescription(getObjectPath(), new String[]{Clock.IFNAME});
+        }
+    }
+}
