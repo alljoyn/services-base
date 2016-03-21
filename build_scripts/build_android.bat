a1b1730c3b8f0b0fb3218df0441ed75fbd593a44
Jarrod Moldrich <jarrod@two-bulls.com>
2016-03-21 18:25:56 +1100

    ASABASE-562 ASABASE-564 Various build fixes and new scripts for the Base Services 16.04 update
    Change-Id: I4b94c5bfe6b249b7fb9fda4b47623c61d5735845
    Signed-off-by: Jarrod Moldrich <jarrod@two-bulls.com>

---
+++ build_scripts/build_android.bat
@@ -0,0 +1,43 @@
+set GTEST_DIR=
+set SCRIPT_FOLDER=%~dp0
+
+rem Defaults
+rem -----------------
+set ALLJOYN_SRC_DIST_DIR=%ALLJOYN_CORE_DIR%\build\%OS%\%CPU%\%VARIANT%\dist
+if "%ALLJOYN_DIST_DIR%"=="" set ALLJOYN_DIST_DIR=%ALLJOYN_SRC_DIST_DIR%
+if "%JOBS%"=="" set JOBS=1
+
+echo Current directory: %CD%
+echo OS: %OS%
+echo CPU: %CPU%
+echo VARIANT: %VARIANT%
+echo BINDINGS: %BINDINGS%
+echo SERVICE: %SERVICE%
+echo ALLJOYN_DIST_DIR: %ALLJOYN_DIST_DIR%
+echo ANDROID_NDK: %ANDROID_NDK%
+echo ANDROID_SDK: %ANDROID_SDK%
+
+rem Build service
+rem ----------------
+pushd %WORKSPACE%\core\alljoyn
+set BUILDDIR=build\%OS%\%CPU%\%VARIANT%\dist
+if exist %BUILDDIR% rmdir /s /q %BUILDDIR%
+mkdir %BUILDDIR%\cpp\lib %BUILDDIR%\cpp\inc
+set CPU=%CPU%
+set ALLJOYN_DISTDIR=%ALLJOYN_DIST_DIR%
+set MSVC_VERSION=%MSVC_VER%
+set V=1
+set OS=%OS%
+set BINDINGS=%BINDINGS%
+set BUILD_SERVICES_SAMPLES=on
+set BR=on
+set DOCS=html
+set WS=%WS%
+set VARIANT=%VARIANT%
+cmd /c %SCONS% "-c" "--jobs=%JOBS%" "OS=%OS%" "BINDINGS=%BINDINGS%" "VARIANT=%VARIANT%" "CPU=%CPU%" "MSVC_VERSION=%MSVC_VER%" "ANDROID_SDK=%ANDROID_SDK%" "ANDROID_NDK=%ANDROID_NDK%"
+cmd /c %SCONS% "--jobs=%JOBS%" "OS=%OS%" "BINDINGS=%BINDINGS%" "VARIANT=%VARIANT%" "CPU=%CPU%" "MSVC_VERSION=%MSVC_VER%" "ANDROID_SDK=%ANDROID_SDK%" "ANDROID_NDK=%ANDROID_NDK%"
+
+IF %ERRORLEVEL% GTR 0 exit %ERRORLEVEL% 
+popd
+
+
