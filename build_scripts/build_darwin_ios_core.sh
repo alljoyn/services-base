022b47f61510566e0f630f49baa4856194bba4bb
Cameron Harvey <cameron+allseen@two-bulls.com>
2016-12-09 09:35:17 +1100

    Replace AllSeenAlliance License with Apache License Version 2.0
    http://www.apache.org/licenses/LICENSE-2.0
    
    * Executed in batch by
      https://cgit.allseenalliance.org/extras/automation.git/tree/relicense/bin/do-relicense
    * Patch review took place here:
      https://git.allseenalliance.org/gerrit/#/c/9377/
    
    * For each branch of each project of the AllJoyn Open Source Project
      gerrit repository
    - search / replace old license with new in all matching files
    - search / replace old copyright with new in all files which only
      include copyright
    - assign copyright to Open Connectivity Foundation, AllJoyn Open
      Source Project Contributors and others.
    - propose changes for review by commiters
    - include SPDX
    
    Change-Id: Icf82404cd73a896943a9899e9669376cd227c155
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- build_scripts/build_darwin_ios_core.sh
+++ build_scripts/build_darwin_ios_core.sh
@@ -1,17 +1,30 @@
 #!/bin/bash -xe
-# Copyright AllSeen Alliance. All rights reserved.
+# #    Copyright (c) Open Connectivity Foundation (OCF) and AllJoyn Open
+#    Source Project (AJOSP) Contributors and others.
 #
-#    Permission to use, copy, modify, and/or distribute this software for any
-#    purpose with or without fee is hereby granted, provided that the above
-#    copyright notice and this permission notice appear in all copies.
+#    SPDX-License-Identifier: Apache-2.0
 #
-#    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-#    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-#    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-#    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-#    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-#    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-#    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#    All rights reserved. This program and the accompanying materials are
+#    made available under the terms of the Apache License, Version 2.0
+#    which accompanies this distribution, and is available at
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Copyright (c) Open Connectivity Foundation and Contributors to AllSeen
+#    Alliance. All rights reserved.
+#
+#    Permission to use, copy, modify, and/or distribute this software for
+#    any purpose with or without fee is hereby granted, provided that the
+#    above copyright notice and this permission notice appear in all
+#    copies.
+#
+#     THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+#     WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+#     WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+#     AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+#     DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+#     PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+#     TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+#     PERFORMANCE OF THIS SOFTWARE.
 #
 
 VARIANT=$1
@@ -20,7 +33,7 @@ VARIANT=$1
 if [ -z $ALLJOYN_DEV ]; then
     cd core/alljoyn/alljoyn_objc
     xcodebuild -project alljoyn_darwin.xcodeproj -scheme alljoyn_core_ios        -sdk iphoneos        -configuration $VARIANT
-    xcodebuild -project alljoyn_darwin.xcodeproj -scheme alljoyn_core_ios        -sdk iphonesimulator -configuration $VARIANT
+    xcodebuild -project alljoyn_darwin.xcodeproj -scheme alljoyn_core_ios        -sdk iphonesimulator -configuration $VARIANT PLATFORM_NAME=iphonesimulator VALID_ARCHS="i386 x86_64"
     xcodebuild -project alljoyn_darwin.xcodeproj -scheme alljoyn_core_ios_armv7  -sdk iphoneos        -configuration $VARIANT
     xcodebuild -project alljoyn_darwin.xcodeproj -scheme alljoyn_core_ios_armv7s -sdk iphoneos        -configuration $VARIANT
     xcodebuild -project alljoyn_darwin.xcodeproj -scheme alljoyn_core_arm64      -sdk iphoneos        -configuration $VARIANT
@@ -33,7 +46,7 @@ fi
 xcodebuild -project alljoyn_about_cpp.xcodeproj -scheme alljoyn_about_cpp -configuration $VARIANT -sdk iphoneos
 
 #$VARIANT version of alljoyn about cpp for iphonesimulator
-xcodebuild -project alljoyn_about_cpp.xcodeproj -scheme alljoyn_about_cpp -configuration $VARIANT -sdk iphonesimulator
+xcodebuild -project alljoyn_about_cpp.xcodeproj -scheme alljoyn_about_cpp -configuration $VARIANT -sdk iphonesimulator PLATFORM_NAME=iphonesimulator VALID_ARCHS="i386 x86_64"
 
 cd ../alljoyn_services_objc
 
@@ -41,7 +54,7 @@ cd ../alljoyn_services_objc
 xcodebuild -project alljoyn_about_objc.xcodeproj -scheme alljoyn_about_objc -configuration $VARIANT -sdk iphoneos
 
 #$VARIANT version of alljoyn about objc for iphonesimulator
-xcodebuild -project alljoyn_about_objc.xcodeproj -scheme alljoyn_about_objc -configuration $VARIANT -sdk iphonesimulator
+xcodebuild -project alljoyn_about_objc.xcodeproj -scheme alljoyn_about_objc -configuration $VARIANT -sdk iphonesimulator PLATFORM_NAME=iphonesimulator VALID_ARCHS="i386 x86_64"
 
 #Library 3 = AllJoynFramework_iOS 
 cd ../../../../../alljoyn_objc/AllJoynFramework_iOS
@@ -50,4 +63,4 @@ cd ../../../../../alljoyn_objc/AllJoynFramework_iOS
 xcodebuild -project AllJoynFramework_iOS.xcodeproj -scheme AllJoynFramework_iOS -configuration $VARIANT -sdk iphoneos
 
 #$VARIANT version of AllJoynFramework_iOS for iphonesimulator
-xcodebuild      -project AllJoynFramework_iOS.xcodeproj -scheme AllJoynFramework_iOS -configuration $VARIANT -sdk iphonesimulator
+xcodebuild      -project AllJoynFramework_iOS.xcodeproj -scheme AllJoynFramework_iOS -configuration $VARIANT -sdk iphonesimulator PLATFORM_NAME=iphonesimulator VALID_ARCHS="i386 x86_64"
